2522bfaa234fb3a77967b4263103c9aef310b41f
Kundan Kumar Sahu <espkundan@gmail.com>
2023-07-06 09:07:02 +0530

    test pre-commit

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,14 @@
 /** @type {import("prettier").Config} */
 const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: false,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
 
